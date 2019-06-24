7e26f29ab9a3b9bb2b0e82d691dbbd734ad590d8
Ethan Jurman <ethanjurman@gmail.com>
2019-06-24 01:48:57 -0400

    type badge effectiveness

--- src/components/TypeBadge/TypeBadge.js
+++ src/components/TypeBadge/TypeBadge.js
@@ -5,7 +5,15 @@ const html = registerHtml()
 
 export default (props) => {
   const displayName = props.short ? props.type.slice(0,3) : props.type;
+  if (props.effectiveness) {
+    return html`
+    <span>
+      <span classname=${`TypeBadgeLeft type-${props.type}`}> ${displayName} </span>
+      <span classname=${`TypeBadgeRight level-${props.effectiveness}`}>${props.effectiveness}</span>
+    </span>
+    `
+  }
   return html`
-    <span classname=${`TypeBadge type-${props.type}`}>${displayName} ${props.effectiveness}</span>
+    <span classname=${`TypeBadge type-${props.type}`}>${displayName}</span>
   `
 }
