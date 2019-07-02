10c5f3e6ef5bb73c3ff9b1f2b2d485fbbcab12aa
Ethan Jurman <ethanjurman@gmail.com>
2019-07-02 18:09:30 -0400

    image strech bug fix

--- src/components/PageFooter/PageFooter.js
+++ src/components/PageFooter/PageFooter.js
@@ -6,7 +6,7 @@ const html = registerHtml()
 export default () => {
   return html`
     <footer class="PageFooter">
-      <a href="https://github.com/JRJurman/team-dex">Team-Dex Created By Ethan & Jesse Jurman</a> using <a href="https://tram-one.io">Tram-One</a>
+      <a href="https://github.com/JRJurman/team-dex">Team-Dex</a> created by Ethan & Jesse Jurman using <a href="https://tram-one.io">Tram-One</a> and the <a href="https://pokeapi.co/">PokéAPI</a>
     </footer>
   `
 }
